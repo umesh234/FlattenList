f6e4490f87ac6f316abe7410fae631ee38ec496b
umesh <umesh@umesh-THINK>
2015-12-27 22:34:13 -0800

    Added Simple unit test that seems to work

--- FlattenList/src/Driver.java
+++ FlattenList/src/Driver.java
@@ -3,7 +3,12 @@ public class Driver {
 
 	public static void main(String args[]) {
 		System.out.println("Hello world!");
-	
+		Node head = Node.makeSample(6, true);
+		Node.printList(head);
+		ListFlatten.flatten(head);
+		Node.printList(head);
+		
 	}
 	
+
 }
--- FlattenList/src/Node.java
+++ FlattenList/src/Node.java
@@ -2,15 +2,42 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 
 
 @AllArgsConstructor
-@EqualsAndHashCode
 public class Node {
+
+	public String printNode() {
+		return "Node [value=" + value + ", prev=" + prev + ", next=" + next
+				+ ", child=" + child + "]";
+	}
 
 	@Getter @Setter int value;
 	@Getter @Setter Node prev;
 	@Getter @Setter Node next;
 	@Getter @Setter Node child;
+	
+	public static void printList(Node head){
+		while(head != null) {
+			System.out.println( head.printNode() + " , ");
+			head = head.next;
+		}
+	}
+	
+
+	static int nodeVal = 0;
+	public static Node makeSample(int numNodes, boolean createChildren) {
+		Node head = new Node(nodeVal++,null, null,null);
+		Node end = head;
+		for(int i=1;i<numNodes;i++){
+			end.next = new Node(nodeVal++,null,null,null);
+			end.next.prev = end;
+			if(createChildren)
+				end.child = makeSample(3, false); 
+			end = end.next;
+		}
+		return head;
+	}
 }
